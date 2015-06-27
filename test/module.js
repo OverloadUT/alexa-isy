1adaea8ef3218b547c6b1aef86a21e52809f001a
Greg Laabs <OverloadUT@gmail.com>
2015-06-26 18:36:45 -0700

    Switch isy99 module to use npm now that it's published

--- test/module.js
+++ test/module.js
@@ -251,24 +251,3 @@ describe('Adjust Device', function () {
         });
     });
 });
-
-// describe('Get Device', function(){
-//     var isy = require('../index.js')();
-
-//     before(function(done){
-//         sinon
-//             .stub(isy._restClient, "get", function(device, callback){
-//                 callback('<nodeInfo><node flag="128"><address>12 34 56 7</address><name>Device Name</name><parent type="3">8555</parent><type>1.32.64.0</type><enabled>true</enabled><deviceClass>0</deviceClass><wattage>0</wattage><dcPeriod>0</dcPeriod><pnode>1C AB 43 1</pnode><ELK_ID>A03</ELK_ID><property id="ST" value="255" formatted="On" uom="%/on/off"/></node><properties><property id="OL" value="255" formatted="100" uom="%/on/off"/><property id="RR" value="28" formatted="0.5 " uom="seconds"/><property id="ST" value="255" formatted="On" uom="%/on/off"/></properties></nodeInfo>', "");
-//             });
-//         done();
-//     });
-//     it('Should get Device info from API', function(done) {
-//         isy.getDeviceInfo("12 34 56 7", function(err, device){
-//             expect(device.address).to.equal("12 34 56 7");
-//             expect(device.name).to.equal("Device Name");
-//             expect(device.status).to.equal('255');
-//             expect(device.statusFormatted).to.equal("On");
-//             done();
-//         });
-//     });
-// });
